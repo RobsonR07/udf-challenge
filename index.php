b0980442d9a073674adaceb9c4dc9e207583f197
RobsonR07 <44735328+RobsonR07@users.noreply.github.com>
2018-11-08 10:52:53 -0200

    Atualização UDF 08-11
    Criação da pasta

---
+++ index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<!--
+     //---------------------------------------------------//
+    //----------Feito Por: Robson/Zé Wilson/Chris--------//
+   //----------Data: 08/11/2018 - 28/11/2018------------//
+  //----------Linguagem: HTML/CSS/JS/SQL/Boostrap------//
+ //----------Titulo: Sistema Desafios-----------------//
+//---------------------------------------------------//   
+-->
+<html lang="PT-BR">
+  <head>
+    <title>UDF Challenger</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--<link rel="icon" href="myicon.ico" type="">-->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#482c58;">
+  <img src="./img/logov2.png" style="margin-right:2%"/>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a id="dog" class="nav-link" href="index.php">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a id="dog" class="nav-link" href="#">Professor</a>
+      </li>
+      <li class="nav-item">
+        <a id="dog" class="nav-link" href="#">Aluno</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!--
+<div class="container-fluid" style="/*margin-top: 5%*/">
+    <div class="row">
+        <div class="col-xl-12">
+        <?php
+        include("config.php");
+?>
+        </div>
+    </div>
+</div>
+-->
+<footer id="meu-rodape">
+    <div class="container">
+      <div class="row">
+        
+      </div>
+    </div>
+    <div class="footer-copyright" style="margin-top: -3%">
+      <p>© 2019 Copyright Registred on Acre in year 1 A.C </p>
+    </div> 
+</footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<style>
+#dog{
+  border: none;
+  color: #fff;
+  padding: 5px 15px;
+  text-align: left;
+  font-size: 16px;
+  margin: 4px 2px;
+  opacity: 0.6;
+}
+
+#dog:hover{
+  opacity: 1;
+  background-color: #fff;
+  transition: 1.25s;
+  color: #4f4e4e;
+  border-radius:10px;
+  }
+</style>
